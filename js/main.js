bbd7f80854e86bf1008a7b1a46d3ff95cea1219d
Mitchell Coleman Holmes <51342931+41Holmes41@users.noreply.github.com>
2019-07-29 10:48:37 -0500

    updates game logic

--- js/main.js
+++ js/main.js
@@ -6,28 +6,33 @@ let COLORS = {
 };
 
 //defining variables
-let win1= [0,1,2];
-let win2= [3,4,5];
-let win3= [6,7,8];
-let win4= [0,3,6];
-let win5= [1,4,7];
-let win6= [2,5,8];
-let win7= [0,4,8];
-let win8= [2,4,6];
+let winArray = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
-let board = [[1,2,3],[4,5,6],[7,8,9]];
+let board;
 
 let turn= null;
 
-let winner = null;
+let winner = 0;
 
 //store play square elements
 
 let squares= document.querySelectorAll('section div');
-console.log(squares);
 
 //event listeners
-document.querySelector('section').addEventListener('click', handleClick)
+document.querySelector('section').addEventListener('click', handleClick);
+document.querySelector('button').addEventListener('click', function() {
+    
+    init();
+});
 
 //functions
 init();
@@ -35,32 +40,79 @@ init();
 function init() {
     board = [0,0,0,0,0,0,0,0,0];
     turn = 1;
+    winner = 0;
+
     render();
 }
 
 function render() {
     //render the board
-/*     board.forEach(function(colArr, rowIdx) {
-        colArr.forEach(function(cell, colIdx){
-            let div= document.getElementById(`c${colIdx}r${rowIdx}`);
-            div.style.backgroundColor=COLORS[cell]
-        });
-    });
- */
+    if ( winner != 0) {
+        return;
+    };
 
    for(let i=0; i<9; i++) {
       let B=  document.querySelectorAll('section div');
       let C = B.item(i);
       C.style.backgroundColor=COLORS[board[i]];
    }
-}
+
+    winCheckPass(winArray); 
+
+
+    if (winner !=0){
+
+        turnDisplay = document.querySelector('h2');
+
+        if (winner === 1) {
+            turnDisplay.innerHTML = `Player 1 wins!`;
+        } else {
+            turnDisplay.innerHTML = `Player 2 wins!`; 
+        }
+
+   } else {
+       turnDisplay = document.querySelector('h2');
+       if (turn === 1) {
+        turnDisplay.innerHTML = `Player 1's turn!`;
+        } else {
+            turnDisplay.innerHTML = `Player 2's turn!`;
+        };
+   };
+};
 
 function handleClick(evt) {
+
+    
     let target = evt.target.id;
     let target2= document.getElementById(target).dataset.square;
-    board[target2]=turn;
-    turn = turn *-1;
+
+    if ( Math.abs(board[target2]) != 1 ) {
+        board[target2]=turn;
+        turn = turn *-1;
+    };
+    //console.log(target2);
     
     render();
 
 }
+
+ function winCheckPass(winArray2) {
+    winCheck(winArray2[0]);
+    winCheck(winArray2[1]);
+    winCheck(winArray2[2]);
+    winCheck(winArray2[3]);
+    winCheck(winArray2[4]);
+    winCheck(winArray2[5]);
+    winCheck(winArray2[6]);
+    winCheck(winArray2[7]);
+}; 
+
+function winCheck(winCombo) {
+    if ( board[winCombo[0]] + board[winCombo[1]] + board[winCombo[2]] === 3 ) {
+        winner = 1;
+    };
+
+    if ( board[winCombo[0]] + board[winCombo[1]] + board[winCombo[2]] === -3 ) {
+        winner = -1;
+    };
+};
